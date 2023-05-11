32b6939f5b7f986903294548c17c62499b878cb2
Kee-Reall <kirill_bezrodny@mail.ru>
2023-05-12 02:39:33 +0500

    init

--- src/Config/config.service.ts
+++ src/Config/config.service.ts
@@ -1,11 +1,18 @@
+import { Logger } from '@nestjs/common';
+
 export class ConfigService {
+  private loger = new Logger(this.constructor.name);
   constructor(private env: NodeJS.ProcessEnv) {}
 
   getPort() {
-    return parseInt(this.env.PORT, 10);
+    return parseInt(process.env.PORT, 10) || 4200;
   }
 
   getUri() {
     return this.env.PG_URL_MASTER;
+  }
+
+  getInitCallback() {
+    return () => this.loger.log(`listening port: ${this.getPort()}`);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from './Config';
 
 const logger = new Logger('Main');
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const port = parseInt(process.env.PORT, 10) || 4200;
-  await app.listen(port, () => logger.log(`Listening port: ${port}`));
+  const configService = app.get(ConfigService);
+  await app.listen(configService.getPort(), configService.getInitCallback());
 }
 bootstrap();
 
