ac346d246d2bf9a0e8052743a57e37806ce01104
Kee-Reall <kirill_bezrodny@mail.ru>
2023-05-12 01:04:55 +0500

    types

--- src/Database/database.provider.ts
+++ src/Database/database.provider.ts
@@ -1,9 +1,10 @@
 import { FactoryProvider, InjectionToken } from '@nestjs/common';
 import { AppDataSource } from './database.source';
+import { DataSource } from 'typeorm';
 
 export const DATABASE_CONNECTION: InjectionToken = 'DATABASE_CONNECTION';
 
-export const databaseProvider: FactoryProvider = {
+export const databaseProvider: FactoryProvider<DataSource> = {
   provide: DATABASE_CONNECTION,
   useFactory: async () => await AppDataSource.initialize(),
 };
