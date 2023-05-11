966b3d692918aab7ebc663e32fecae12d1fe4d61
Kee-Reall <kirill_bezrodny@mail.ru>
2023-05-12 01:04:55 +0500

    types

--- src/Config/config.provider.ts
+++ src/Config/config.provider.ts
@@ -1,7 +1,7 @@
 import { ConfigService } from './config.service';
 import { FactoryProvider } from '@nestjs/common';
 
-export const configProvider: FactoryProvider = {
+export const configProvider: FactoryProvider<ConfigService> = {
   provide: ConfigService,
   useFactory: async () => new ConfigService(process.env),
 };
