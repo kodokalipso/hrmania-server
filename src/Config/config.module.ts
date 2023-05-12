5876f0b07a8c498467eab5ef82aebd5423cc737d
Kee-Reall <kirill_bezrodny@mail.ru>
2023-05-12 11:55:04 +0500

    init

--- src/Config/config.module.ts
+++ src/Config/config.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { configProvider } from './config.provider';
+import { ConfigService } from './config.service';
 
 @Module({
   providers: [configProvider],
-  exports: [configProvider],
+  exports: [ConfigService],
 })
 export class ConfigModule {}
