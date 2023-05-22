34499e275319eaa3d331a2b2c7901697f390d173
Kee-Reall <kirill_bezrodny@mail.ru>
2023-05-23 00:42:23 +0500

    Start commit

---
+++ src/Base/patterns/layer-contract.class.ts
@@ -0,0 +1,53 @@
+import { Nullable, Nullish } from '../types';
+import { isNil } from '@nestjs/common/utils/shared.utils';
+
+export class ContractExeption extends Error {
+  constructor(msg?: string) {
+    super(`ERROR DURING CONTRACT LIFECYCLE \n ${msg ?? 'UNDEFINED MESSAGE'}`);
+  }
+}
+export class LayerContract<T = unknown> {
+  protected statusCode = 1;
+  protected statusDescription: Nullish<string>;
+  protected payload: T;
+  protected process: Nullable<boolean> = null;
+
+  protected static SUCCESS_CODE = 0;
+
+  public setSuccess(payload) {
+    if (isNil(this.process)) {
+      throw new ContractExeption('ALREADY SET');
+    }
+    this.payload = payload;
+    this.statusCode = LayerContract.SUCCESS_CODE;
+    this.process = true;
+  }
+
+  public setFailed(code = 1, description?: Nullish<string>) {
+    if (isNil(this.process)) {
+      throw new ContractExeption('ALREADY SET');
+    }
+    this.statusCode = code;
+    this.statusDescription = description;
+    this.process = false;
+  }
+
+  public getCode() {
+    return this.statusCode;
+  }
+
+  public getDescription() {
+    return this.statusDescription;
+  }
+
+  public getInfo() {
+    return { code: this.statusCode, description: this.statusDescription };
+  }
+
+  public getPayload() {
+    if (!isNil(this.process)) {
+      throw new ContractExeption('PAYLOAD');
+    }
+    return this.payload;
+  }
+}
